f180e48cdd35adee5949ff87b17a07a4162811e7
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-09-20 11:30:34 -0300

    application start

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800
+    })
+
+    win.loadFile('src/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'win32') app.quit()
+})
+
